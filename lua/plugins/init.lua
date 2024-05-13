1f23473794ded51853f581b0dd98281a648f1144
Minki Ha <minkiha@protonmail.com>
2024-05-13 02:57:50 -0400

    supermaven-nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,11 +64,18 @@ return {
   },
   {
     "folke/trouble.nvim",
+    lazy = false,
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     },
+  },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup {}
+    end,
   },
 }
